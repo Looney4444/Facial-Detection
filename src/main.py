e22c9e381d805d072f09a0c286503024e51f1456
Albert Looney <alooney@capital.edu>
2015-09-07 01:59:47 -0400

    Added Smile Detection

--- src/main.py
+++ src/main.py
@@ -4,10 +4,15 @@
 
 import cv2
 
-cascadePath = 'haarcascade_frontalface_default.xml'
-faceCascade = cv2.CascadeClassifier(cascadePath)
+cascadePath_Face = './classifiers/haarcascade_frontalface_default.xml'
+cascadePath_Smile = './classifiers/haarcascade_smile.xml'
+
+faceCascade = cv2.CascadeClassifier(cascadePath_Face)
+smileCascade = cv2.CascadeClassifier(cascadePath_Smile)
+
 
 video_capture = cv2.VideoCapture(0)
+
 
 while True:
     # Capture frame-by-frame
@@ -19,12 +24,23 @@ while True:
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(30, 30)
+        minSize=(15, 15)
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        smiles = smileCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor=1.1,
+            minNeighbors=600,
+            minSize=(10, 10)
+        )
+
+        for (ex, ey, ew, eh) in smiles[:1]:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 255), 2)
 
     # Display the resulting frame
     cv2.imshow('Facial Detection', frame)
