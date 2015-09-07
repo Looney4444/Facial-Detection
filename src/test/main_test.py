08d7c9b750560736bb862576fcc562f2edd35dc6
Albert Looney <alooney@capital.edu>
2015-09-07 03:21:19 -0400

    Started Implementing Unit Test with Travis Build

---
+++ src/test/main_test.py
@@ -0,0 +1,12 @@
+__author__ = 'Albert Looney'
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
